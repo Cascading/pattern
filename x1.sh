3ec3b4eee3e566e632610679a66da9709a949275
Paco Nathan <ceteri@gmail.com>
2012-10-20 16:10:41 -0700

    cleaning out unneeded stuff

--- x1.sh
+++ x1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CLASSPATH=.:./lib/janino.jar:./lib/commons-compiler.jar:./build
+CLASSPATH=.:./lib/janino.jar:./lib/commons-compiler.jar:./lib/jgrapht-jdk1.6.jar:./build
 
-javac -d build src/java/XPathReader.java src/java/XPathExample.java
+javac -d build src/java/XPathReader.java
 
 java XPathReader data/sample.xml
