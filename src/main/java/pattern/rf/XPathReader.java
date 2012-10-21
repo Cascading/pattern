93c80d2aae8e36c96e9de3c93c3d2d2f80528c72
Paco Nathan <ceteri@gmail.com>
2012-10-21 09:45:49 -0700

    refactor for Gradle build

--- src/main/java/pattern/rf/XPathReader.java
+++ src/main/java/pattern/rf/XPathReader.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2007-2012 Concurrent, Inc. All Rights Reserved.
+ *
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pattern.rf;
+
 import java.io.IOException;
 import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
@@ -12,8 +34,9 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
  
-public class XPathReader
-{
+public class
+  XPathReader
+  {
   private String xmlFile;
   private Document xmlDocument;
   private XPath xPath;
--- x1.sh
+++ x1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 CLASSPATH=.:./lib/janino.jar:./lib/commons-compiler.jar:./lib/jgrapht-jdk1.6.jar:./build
+SRC=src/main/java/pattern/rf
 
-javac -d build src/java/XPathReader.java
-javac -d build src/java/Vertex.java
-javac -d build src/java/Edge.java
-javac -d build src/java/Tree.java
-javac -d build src/java/RandomForest.java
+javac -d build $SRC/XPathReader.java
+javac -d build $SRC/Vertex.java
+javac -d build $SRC/Edge.java
+javac -d build $SRC/Tree.java
+javac -d build $SRC/RandomForest.java
 
-java RandomForest data/sample.xml data/sample.tsv
+java pattern.rf.RandomForest data/sample.xml data/sample.tsv
